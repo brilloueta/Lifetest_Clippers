7bce633431bb04189942c7affed3a5897bd7f272
brilloueta <alexandre_brillouet@conair.com>
2017-07-30 22:34:35 +0200

    update tests

--- tests.py
+++ tests.py
@@ -4,16 +4,21 @@
 import lifetest_FX811E_BCM as lt
 import time
 
-#lt.log_counter(30)
-#lt.load_app_state()
-#lt.set_cycles(8)
-#lt.set_pas_incr(16)
+lt.log_to_csv(30)
+lt.load_app_state()
+lt.set_cycles(8)
+lt.set_pas_incr(16)
 
-def date_str():
-    return time.time()
+lt.log_to_csv(60)
+lt.load_app_state()
+lt.set_cycles(16)
+lt.set_pas_incr(32)
 
-print('seconds since epoch : ', date_str())
-print('date since epoch : ', time.strftime('%d/%m/%y %H:%M:%S', time.localtime(date_str())))
+print('seconds since epoch : ', lt.date_sec_epoch())
+print('date since epoch : ', time.strftime('%d/%m/%y %H:%M:%S', time.localtime(lt.date_sec_epoch())))
+print('\n')
 
-print ('current_day = ', time.strftime('%A',time.localtime(date_str())))
-print ('current_hour = ', int(time.strftime('%H',time.localtime(date_str()))))
+print ('current_day = ', time.strftime('%A',time.localtime(lt.date_sec_epoch())))
+print ('current_hour = ', int(time.strftime('%H',time.localtime(lt.date_sec_epoch()))))
+print('\n')
+
