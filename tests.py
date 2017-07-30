0287fe80326e570485e73c0bbb5c2f8267c2c66d
brilloueta <alexandre_brillouet@conair.com>
2017-07-30 21:48:58 +0200

    update tests

--- tests.py
+++ tests.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-#
 
 import lifetest_FX811E_BCM as lt
+import time
 
-lt.load_app_state()
-lt.set_cycles(13)
-lt.set_pas_incr(17)
+#lt.log_counter(30)
+#lt.load_app_state()
+#lt.set_cycles(8)
+#lt.set_pas_incr(16)
+
+def date_str():
+    return time.time()
+
+print('seconds since epoch : ', date_str())
+print('date since epoch : ', time.strftime('%d/%m/%y %H:%M:%S', time.localtime(date_str())))
+
+print ('current_day = ', time.strftime('%A',time.localtime(date_str())))
+print ('current_hour = ', int(time.strftime('%H',time.localtime(date_str()))))
